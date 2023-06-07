cbfb12034a9e45549e1f5c655a294fab2aa4635e
Pradeep_sain_03 <130055570+PradeepSain03@users.noreply.github.com>
2023-06-07 17:25:31 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# React-Ecommerce-website
+
+
+### Front-End
+* A landing page with animated minimalistic design, with the major categories of items.
+* A quick peek page where 5 products of each categories are on display.
+* A page with Sign-in and Sign-out components.
+* All form related fields are made using reusable components.
+* Has a cart feature which opens up and displays the items selected in a summary, also has a checkout button.
+* The cart component also allows to edit the contents of the items selected.
+* Check out features are yet to be integrated into the appplication. 
+
+
+
+
+* Install node modules
+
+   ```bash
+   npm install
+   ```
+
+
+### Starting front-end servers
+
+* Build application
+
+  ```bash
+  npm start
+  ```
+---
+
+
+
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,80 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return
+
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get()
+    // console.log(snapShot)
+
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log("error creating user", error.message)
+        }
+    }
+    return userRef
+}
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey)
+    // console.log(collectionRef)
+    const batch = firestore.batch()
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc()
+        // console.log(newDocRef)
+        batch.set(newDocRef, obj)
+    })
+    return await batch.commit()
+}
+
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollections = collections.docs.map(doc => {
+        const { title, items } = doc.data()
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    })
+    return transformedCollections.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection
+        return accumulator
+    }, {})
+}
+
+
+
+
+
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+//google authentication
+
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({ prompt: 'select_account' })
+
+export const signInWithGoogle = () => {
+    auth.signInWithPopup(provider)
+}
+
+export default firebase
